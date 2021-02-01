c78265ff51033dd7a0f095fe351bd409411ba705
PatrykPas <235948@edu.p.lodz.pl>
2021-02-01 23:04:51 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GithuuubTrain.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GithuuubTrain.dir/main.cpp.obj"
+  "GithuuubTrain.exe"
+  "GithuuubTrain.exe.manifest"
+  "GithuuubTrain.pdb"
+  "libGithuuubTrain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GithuuubTrain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
